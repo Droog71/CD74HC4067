755751e00895ab333db79d8e6c79c9fa47fb9d2d
Maverick <71droog71@gmail.com>
2023-06-05 22:11:12 -0400

    Added breakout svg and json. Fixed incorrect pin_init for VCC, GND and COM. Added check for VCC and GND connections. Added off state when E pin is high.

--- src/main.c
+++ src/main.c
@@ -23,11 +23,17 @@ typedef struct {
   uint32_t analog_demux;
 } chip_data_t;
 
+bool power_connected(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  return pin_read(chip->VCC) && !pin_read(chip->GND);
+}
+
 void chip_timer_callback(void *data) 
 {
   chip_data_t *chip = (chip_data_t*)data;
 
-  if (!pin_read(chip->E))
+  if (power_connected(chip) && !pin_read(chip->E))
   {
     bool S0 = pin_read(chip->S[0]);
     bool S1 = pin_read(chip->S[1]);
@@ -71,6 +77,21 @@ void chip_timer_callback(void *data)
       }
     }
   }
+  else
+  {
+    chip->COM = pin_init("COM", ANALOG);
+    pin_dac_write(chip->COM, 0);
+      
+    for (int i = 0; i < 16; ++i)
+    {
+      chip->I[i] = pin_init(pin_names_I[i], ANALOG);
+    }
+
+    for (int i = 0; i < 16; ++i)
+    {
+      pin_dac_write(chip->I[i], 0);
+    }
+  }
 }
 
 void chip_init() 
@@ -87,9 +108,9 @@ void chip_init()
   chip->S[2] = pin_init("S2", INPUT);
   chip->S[3] = pin_init("S3", INPUT);
 
-  chip->E = pin_init("COM", INPUT);
-  chip->E = pin_init("VCC", INPUT);
-  chip->E = pin_init("GND", INPUT);
+  chip->COM = pin_init("COM", INPUT);
+  chip->VCC = pin_init("VCC", INPUT);
+  chip->GND = pin_init("GND", INPUT);
   chip->E = pin_init("E", INPUT);
 
   chip->analog_demux = attr_init("analog_demux", 0);
